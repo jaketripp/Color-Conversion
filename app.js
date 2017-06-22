823a4548e143b8f377772b812b715123c378b93c
Jake Tripp <shmogens@gmail.com>
2017-06-21 23:35:16 -0500

    add more random colors, improve ui

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
 var nums = {1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0'}
-var randomColors = ["#1ABC9C", "#2ECC71", "#3498DB", "#9B59B6", "#34495E", "#16A085", "#27AE60", "#2980B9", "#8E44AD", "#2C3E50", "#F1C40F", "#E67E22", "#E74C3C", "#F39C12", "#D35400", "#C0392B"];
+var randomColors = ["#1ABC9C", "#2ECC71", "#3498DB", "#9B59B6", "#34495E", "#16A085", "#27AE60", "#2980B9", "#8E44AD", "#2C3E50", "#F1C40F", "#E67E22", "#E74C3C", "#F39C12", "#D35400", "#C0392B", "#CD5C5C", "#F08080", "#FA8072", "#E9967A", "#FFA07A", "#DC143C", "#FF0000", "#B22222", "#8B0000", "#FFC0CB", "#FFB6C1", "#FF69B4", "#FF1493", "#C71585", "#DB7093", "#FFA07A", "#FF7F50", "#FF6347", "#FF4500", "#FF8C00", "#FFA500", "#FFD700", "#FFFF00", "#F0E68C", "#BDB76B", "#D8BFD8", "#DDA0DD", "#EE82EE", "#DA70D6", "#FF00FF", "#FF00FF", "#BA55D3", "#9370DB", "#663399", "#8A2BE2", "#9400D3", "#9932CC", "#8B008B", "#800080", "#4B0082", "#6A5ACD", "#483D8B", "#7B68EE", "#ADFF2F", "#7FFF00", "#7CFC00", "#00FF00", "#32CD32", "#98FB98", "#90EE90", "#00FA9A", "#00FF7F", "#3CB371", "#2E8B57", "#228B22", "#008000", "#006400", "#9ACD32", "#6B8E23", "#808000", "#556B2F", "#66CDAA", "#8FBC8B", "#20B2AA", "#008B8B", "#008080", "#00FFFF", "#00FFFF", "#AFEEEE", "#7FFFD4", "#40E0D0", "#48D1CC", "#00CED1", "#5F9EA0", "#4682B4", "#B0C4DE", "#87CEEB", "#87CEFA", "#00BFFF", "#1E90FF", "#6495ED", "#7B68EE", "#4169E1", "#0000FF", "#0000CD", "#00008B", "#000080", "#191970"]
 
 function getRandomInt(min, max) {
     var min = Math.ceil(min);
@@ -12,7 +12,7 @@ function getRandomInt(min, max) {
 
 // start off with a random background color. give the first input focus
 function randomStartingBackgroundColor(){
-	var randomColor = randomColors[getRandomInt(0, 15)];
+	var randomColor = randomColors[getRandomInt(0, 103)];
 
 	var arr = randomColor.split('');
 
