761b671fd64c20815215a6d2369d18f977331112
Jake Tripp <shmogens@gmail.com>
2017-11-17 14:20:23 -0600

    remove maxlength from inputs on HTML, try and fix JS file, add some es6

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function() {
 
-var nums = {1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0'}
-var randomColors = ["#1ABC9C", "#2ECC71", "#3498DB", "#9B59B6", "#34495E", "#16A085", "#27AE60", "#2980B9", "#8E44AD", "#2C3E50", "#F1C40F", "#E67E22", "#E74C3C", "#F39C12", "#D35400", "#C0392B", "#CD5C5C", "#F08080", "#FA8072", "#E9967A", "#FFA07A", "#DC143C", "#FF0000", "#B22222", "#8B0000", "#FFC0CB", "#FFB6C1", "#FF69B4", "#FF1493", "#C71585", "#DB7093", "#FFA07A", "#FF7F50", "#FF6347", "#FF4500", "#FF8C00", "#FFA500", "#FFD700", "#FFFF00", "#F0E68C", "#BDB76B", "#D8BFD8", "#DDA0DD", "#EE82EE", "#DA70D6", "#FF00FF", "#FF00FF", "#BA55D3", "#9370DB", "#663399", "#8A2BE2", "#9400D3", "#9932CC", "#8B008B", "#800080", "#4B0082", "#6A5ACD", "#483D8B", "#7B68EE", "#ADFF2F", "#7FFF00", "#7CFC00", "#00FF00", "#32CD32", "#98FB98", "#90EE90", "#00FA9A", "#00FF7F", "#3CB371", "#2E8B57", "#228B22", "#008000", "#006400", "#9ACD32", "#6B8E23", "#808000", "#556B2F", "#66CDAA", "#8FBC8B", "#20B2AA", "#008B8B", "#008080", "#00FFFF", "#00FFFF", "#AFEEEE", "#7FFFD4", "#40E0D0", "#48D1CC", "#00CED1", "#5F9EA0", "#4682B4", "#B0C4DE", "#87CEEB", "#87CEFA", "#00BFFF", "#1E90FF", "#6495ED", "#7B68EE", "#4169E1", "#0000FF", "#0000CD", "#00008B", "#000080", "#191970"]
+var nums = {1: '0', 2: '0', 3: '0', 4: '0', 5: '0', 6: '0'};
+var randomColors = ["#1ABC9C", "#2ECC71", "#3498DB", "#9B59B6", "#34495E", "#16A085", "#27AE60", "#2980B9", "#8E44AD", "#2C3E50", "#F1C40F", "#E67E22", "#E74C3C", "#F39C12", "#D35400", "#C0392B", "#CD5C5C", "#F08080", "#FA8072", "#E9967A", "#FFA07A", "#DC143C", "#FF0000", "#B22222", "#8B0000", "#FFC0CB", "#FFB6C1", "#FF69B4", "#FF1493", "#C71585", "#DB7093", "#FFA07A", "#FF7F50", "#FF6347", "#FF4500", "#FF8C00", "#FFA500", "#FFD700", "#FFFF00", "#F0E68C", "#BDB76B", "#D8BFD8", "#DDA0DD", "#EE82EE", "#DA70D6", "#FF00FF", "#FF00FF", "#BA55D3", "#9370DB", "#663399", "#8A2BE2", "#9400D3", "#9932CC", "#8B008B", "#800080", "#4B0082", "#6A5ACD", "#483D8B", "#7B68EE", "#ADFF2F", "#7FFF00", "#7CFC00", "#00FF00", "#32CD32", "#98FB98", "#90EE90", "#00FA9A", "#00FF7F", "#3CB371", "#2E8B57", "#228B22", "#008000", "#006400", "#9ACD32", "#6B8E23", "#808000", "#556B2F", "#66CDAA", "#8FBC8B", "#20B2AA", "#008B8B", "#008080", "#00FFFF", "#00FFFF", "#AFEEEE", "#7FFFD4", "#40E0D0", "#48D1CC", "#00CED1", "#5F9EA0", "#4682B4", "#B0C4DE", "#87CEEB", "#87CEFA", "#00BFFF", "#1E90FF", "#6495ED", "#7B68EE", "#4169E1", "#0000FF", "#0000CD", "#00008B", "#000080", "#191970"];
 
 function getRandomInt(min, max) {
-    var min = Math.ceil(min);
-    var max = Math.floor(max);
+    min = Math.ceil(min);
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
@@ -17,12 +17,12 @@ function randomStartingBackgroundColor(){
 	var arr = randomColor.split('');
 
 	// update dom
-	$('#1').val(arr[1]);
-	$('#2').val(arr[2]);
-	$('#3').val(arr[3]);
-	$('#4').val(arr[4]);
-	$('#5').val(arr[5]);
-	$('#6').val(arr[6]);
+	$('#1').attr('value', arr[1]).val(arr[1]);
+	$('#2').attr('value', arr[2]).val(arr[2]);
+	$('#3').attr('value', arr[3]).val(arr[3]);
+	$('#4').attr('value', arr[4]).val(arr[4]);
+	$('#5').attr('value', arr[5]).val(arr[5]);
+	$('#6').attr('value', arr[6]).val(arr[6]);
 
 	// update nums
 	nums[1] = arr[1];
@@ -39,7 +39,7 @@ function randomStartingBackgroundColor(){
 function reset() {
 	for (var i in nums) {
 		nums[i] = '0';
-		$('#' + i).val('0');
+		$('#' + i).val('0').attr('value', '0');
 	}
 
 	$('#rgb').text('rgb(0,0,0)');
@@ -58,7 +58,10 @@ function setUpHexNums(e) {
 	if (e.key.length === 1) {
 		if (/([0-9a-fA-F]){1}/.test(input.value) && /([0-9a-fA-F]){1}/.test(e.key)) {
 			nums[id] = input.value.toUpperCase();
+			$('#' + id).attr('value', nums[id]);
 			$('#' + id).next().focus().select();
+		} else {
+			$('#' + id).val('');
 		}
 	}
 }
@@ -80,7 +83,14 @@ function calculateRGB() {
 	var domNum6 = parseInt(nums[6], 16);
 	var pair3 = (domNum5 * 16) + domNum6;
 
-	return 'rgb(' + pair1 + ',' + pair2 + ',' + pair3 + ')';
+	return `rgb(${pair1}, ${pair2}, ${pair3})`;
+}
+
+function repopulateEmptyInput(e) {
+	var id = e.target.id;
+	if ($('#' + id).val() === '') {
+		$('#' + id).val(nums[id]);
+	}
 }
 
 // updates background and the text showing the rgb conversion
@@ -94,6 +104,7 @@ function bindEvents() {
 	$('input').on('keyup', setUpHexNums);
 	$('input').on('keyup', updateBackgroundColor);
 	$('#reset').on('click', reset);
+	$('input').on('blur', repopulateEmptyInput);
 }
 
 function init(){
